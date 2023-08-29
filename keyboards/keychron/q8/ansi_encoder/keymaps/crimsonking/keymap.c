575d3a3d25b6991e7013050f18d1e7c39c41ea22
Aleksandar Straumann <iris@pandorum.local>
2023-08-29 09:37:00 -0500

    udpate timeout

--- keyboards/keychron/q8/ansi_encoder/keymaps/crimsonking/keymap.c
+++ keyboards/keychron/q8/ansi_encoder/keymaps/crimsonking/keymap.c
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJUST] = LAYOUT_ansi_69(
         KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   QK_BOOT,          _______,
-        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
+        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, RGB_HUD, RGB_HUI, RGB_RMOD,  RGB_MOD, _______,  _______,  _______,  _______,          _______,
         _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD,           KC_BRIU, KC_MPRV, KC_MPLY, KC_MNXT,  _______,  _______,  _______,          _______,
         _______,           _______,  _______, _______, _______, _______,  KC_BRID, KC_VOLD, KC_MUTE, KC_VOLU,  _______,  _______,  _______, _______,
         _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______)
